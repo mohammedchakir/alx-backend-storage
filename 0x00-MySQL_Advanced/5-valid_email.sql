93c4c5744ab5a71f43e50b5401633a8215f15d16
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-09 11:39:35 +0100

    add corrections

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -2,7 +2,7 @@
 -- when the email has been changed SQL script to create the trigger:
 DELIMITER //
 
-CREATE TRIGGER update_valid_email AFTER UPDATE ON users
+CREATE TRIGGER update_valid_email BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF OLD.email <> NEW.email THEN

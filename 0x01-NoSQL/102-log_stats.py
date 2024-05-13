d422990b600f99ddf6818d6db967b0dd54124da3
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-13 17:10:49 +0100

    add

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -13,24 +13,32 @@ def log_stats():
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_collection = client.logs.nginx
-
-    total_logs = logs_collection.count_documents({})
-    print("{} logs".format(total_logs))
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = logs_collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
-
-    status_check_count = logs_collection.count_documents({"method": "GET",
-                                                          "path": "/status"})
-    print("{} status check".format(status_check_count))
-
-    ip_counts = Counter(log['ip'] for log in logs_collection.find())
-    top_ips = ip_counts.most_common(10)
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{total} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
     print("IPs:")
-    for ip, count in top_ips:
-        print("\t{}: {}".format(ip, count))
+    sorted_ips = logs_collection.aggregate(
+        [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+         {"$sort": {"count": -1}}])
+    i = 0
+    for s in sorted_ips:
+        if i == 10:
+            break
+        print(f"\t{s.get('_id')}: {s.get('count')}")
+        i += 1
 
 
 if __name__ == "__main__":

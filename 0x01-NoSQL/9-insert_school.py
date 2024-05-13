2bee5f95388292219884a53cf20762ed5668c43a
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-13 16:46:14 +0100

    add commit

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """
-Module to insert a new document in a collection based on kwargs.
+Function to insert a new document into a collection based on kwargs.
 """
 
 
 def insert_school(mongo_collection, **kwargs):
     """
-    Insert a new document into a MongoDB collection based on kwargs.
+    Insert a new document into the given MongoDB collection
+    based on the provided kwargs.
     """
-    new_document = kwargs
-    result = mongo_collection.insert_one(new_document)
-    return result.inserted_id
+    return mongo_collection.insert_one(kwargs).inserted_id

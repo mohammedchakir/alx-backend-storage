787c40a33dd3b1906ad6d891c715b6c23bec10d6
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-15 13:51:39 +0100

    commit corrections

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,48 +7,35 @@ import requests
 import redis
 from functools import wraps
 
-
-def cache_with_expiry(seconds):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            url = args[0]
-            cache_key = f"page:{url}"
-            count_key = f"count:{url}"
-
-            # Check if the page content is cached
-            cached_content = cache.get(cache_key)
-            if cached_content:
-                # If cached, increment access count
-                cache.incr(count_key)
-                return cached_content.decode("utf-8")
-
-            # If not cached, fetch the page content
-            page_content = func(*args, **kwargs)
-
-            # Cache the page content with an expiration time
-            cache.setex(cache_key, seconds, page_content)
-
-            # Initialize access count if not present
-            cache.setnx(count_key, 0)
-            # Increment access count
-            cache.incr(count_key)
-
-            return page_content
-
-        return wrapper
-
-    return decorator
+r = redis.Redis()
 
 
-# Connect to Redis
-cache = redis.Redis(host='localhost', port=6379, db=0)
+def url_access_count(method):
+    """decorator for get_page function"""
+    @wraps(method)
+    def wrapper(url):
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+            # Get new content and update cache
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
+        return html_content
+    return wrapper
 
 
-@cache_with_expiry(10)
+@url_access_count
 def get_page(url: str) -> str:
-    response = requests.get(url)
-    return response.text
+    """obtain the HTML content of a particular"""
+    results = requests.get(url)
+    return results.text
 
 
 if __name__ == "__main__":

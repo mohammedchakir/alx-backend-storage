5e1d85992e5b0be5805d7510fefcb24e5a0fd712
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-15 13:58:04 +0100

    add

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,37 +3,38 @@
 code for the web.py file implementing the get_page
 function with caching and access
 """
-import requests
 import redis
+import requests
 from functools import wraps
-from typing import Callable
 
-redis = redis.Redis()
+r = redis.Redis()
 
 
-def wrap_requests(fn: Callable) -> Callable:
-    """ Decorator wrapper """
-
-    @wraps(fn)
+def url_access_count(method):
+    """decorator for get_page function"""
+    @wraps(method)
     def wrapper(url):
-        """ Wrapper for decorator guy """
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-        result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
-        return result
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
 
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
+        return html_content
     return wrapper
 
 
-@wrap_requests
+@url_access_count
 def get_page(url: str) -> str:
-    """get page self descriptive
-    """
-    response = requests.get(url)
-    return response.text
+    """obtain the HTML content of a particular"""
+    results = requests.get(url)
+    return results.text
 
 
 if __name__ == "__main__":

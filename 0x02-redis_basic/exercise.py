05de83f9dfd872d0a40b69cdf986cdedfa8afbd4
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-15 12:56:12 +0100

    commit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Cache class for storing data in Redis.
 """
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 import redis
 
 
@@ -14,33 +14,52 @@ class Cache:
 
     def __init__(self) -> None:
         """
-        Initializes the Cache class with a Redis client
-        and flushes the Redis instance.
+        Initializes the Cache class with a Redis client and flushes the Redis instance.
         """
         self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Generates a random key and stores the input data in
-        Redis using the key.
-        Args:
-            data (Union[str, bytes, int, float]): The data to be
-            stored in the cache.
-        Returns:
-            str: The randomly generated key used for storing the
-            data in Redis.
+        Generates a random key and stores the input data in Redis using the key.
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn: Optional[Callable[[bytes], Union[str, int, float]]] = None) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieves data from Redis using the given key and
+        optionally applies a conversion function.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Retrieves data from Redis using the given key and converts it to a string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Retrieves data from Redis using the given key and converts it to an integer.
+        """
+        return self.get(key, fn=int)
 
 if __name__ == "__main__":
     cache = Cache()
-    data = b"hello"
-    key = cache.store(data)
-    print(key)
 
-    local_redis = redis.Redis()
-    print(local_redis.get(key))
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value

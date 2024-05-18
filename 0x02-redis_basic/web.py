9c5d9ee3b0e9e2698c947f0d8cb61266347df547
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-18 03:20:47 +0100

    add

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -14,7 +14,6 @@ an expiration time of 10 seconds.
 Tip: Use http://slowwly.robertomurray.co.uk to simulate
 a slow response and test your caching."""
 
-
 import redis
 import requests
 from functools import wraps
@@ -22,31 +21,35 @@ from functools import wraps
 r = redis.Redis()
 
 
-def url_access_count(method):
-    """decorator for get_page function"""
-    @wraps(method)
-    def wrapper(url):
-        """wrapper function"""
-        key = "cached:" + url
-        cached_value = r.get(key)
-        if cached_value:
-            key_count = "count:" + url
-            r.incr(key_count, 1)  # Increment count
-            return cached_value.decode("utf-8")
+def cache_with_expiry(seconds):
+    """Decorator to cache function results with expiration"""
+    def decorator(func):
+        @wraps(func)
+        def wrapper(url):
+            """Wrapper function"""
+            key = f"cached:{url}"
+            count_key = f"count:{url}"
+            cached_value = r.get(key)
+            if cached_value:
+                # If cached value exists, update count and return cached value
+                r.incr(count_key, 1)
+                return cached_value.decode("utf-8")
 
-        # Get new content and update cache
-        key_count = "count:" + url
-        html_content = method(url)
+            # If not cached, fetch new content
+            html_content = func(url)
 
-        r.incr(key_count, 1)  # Increment count
-        r.setex(key, 10, html_content)  # Set cache with expiration time
-        return html_content
-    return wrapper
+            # Set cache with expiration time and update count
+            r.setex(key, seconds, html_content)
+            r.incr(count_key, 1)
+
+            return html_content
+        return wrapper
+    return decorator
 
 
-@url_access_count
+@cache_with_expiry(10)
 def get_page(url: str) -> str:
-    """obtain the HTML content of a particular"""
+    """Function to obtain the HTML content of a URL"""
     results = requests.get(url)
     return results.text
 
